4f333efe2484abf779787335411156c4a1ffa2fc
Alan <alan723@hotmail.fr>
2019-05-22 09:26:39 +0200

    Quelques templates en plus

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use App\Entity\Utilisateur;
+use App\Entity\Formation;
 use App\Repository\FormationRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -24,9 +29,30 @@ class HomeController extends AbstractController
     /**
      * @Route("/formation/new", name="formation_create")
      */
-    public function createFormation()
+    public function createFormation(Request $request, ObjectManager $manager)
     {
+        $formation = new Formation();
 
+        $form = $this->createFormBuilder($formation)
+            ->add('parcoursFormation', TextType::class)
+            ->add('dateDebut', DateType::class)
+            ->add('lieu', ChoiceType::class, [
+                'choices' => [
+                    'Nantes' => 0,
+                    'Le Mans' => 1,
+                    'Rennes ' => 3,
+                    'Laval' => 4,
+                    'Niort' => 5,
+                    'Roche sur Yon' => 6,
+                    'Angers' => 7,
+                    'Quimper' => 8
+                ]
+            ])
+            ->getForm();
+
+        return $this->render('formation/createFormation.html.twig', [
+            'formFormation' => $form->createView()
+        ]);
     }
 
 }
