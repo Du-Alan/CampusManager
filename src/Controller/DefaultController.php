3b5d15654b60a088dda6fbb0bcef7bb071213eb6
DWWM <alan723@hotmail.fr>
2019-06-12 19:21:05 +0200

    Quelques modifs visuelle page d'accueil -> login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -14,8 +14,6 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        return $this->redirectToRoute('login');
     }
 }
